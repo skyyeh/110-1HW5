91d091e2e93a82849a0687468af8dd5896abedb6
于翔 林 <108111120@mail.aeust.edu.tw>
2021-11-03 15:34:51 +0800

    HW5

--- 110-1HW5/DropdownListShow.aspx.cs
+++ 110-1HW5/DropdownListShow.aspx.cs
@@ -5,9 +5,22 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace _110_1HW5 {
-    public partial class DropdownListShow : System.Web.UI.Page {
-        protected void Page_Load(object sender, EventArgs e) {
+namespace _110_1HW5
+{
+    public partial class DropdownListShow : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ddl_Area.Items.Add("北區");
+            ddl_Area.Items.Add("南區");
+
+            ddl_Place.Items.Add("基隆");
+            ddl_Place.Items.Add("台北");
+            ddl_Place.Items.Add("新北");
+            ddl_Place.Items.Add("苗栗");
+            ddl_Place.Items.Add("台中");
+            ddl_Place.Items.Add("南投");
+
 
         }
     }
